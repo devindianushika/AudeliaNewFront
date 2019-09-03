4265815fa46dbe6040965dc955cc5f1898f501e6
RizkyRajitha <rajithagunathilake@gmail.com>
2019-09-03 21:08:16 +0530

    added assning and studend dahsboard

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import AdminDashBoard from './containers/Admin/DashBoard/AdminDashBoard';
-import LoginPage from './containers/Auth/LoginPage/LoginPage';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import AdminDashBoard from "./containers/Admin/DashBoard/AdminDashBoard";
+import LoginPage from "./containers/Auth/LoginPage/LoginPage";
+import Studentdashboard from "./pages/studentdashboard/studentdashboard";
+import AdminNavigationBar from "./components/AdminNavigationBar/AdminNavigationBar";
 
 function App() {
   return (
     <div>
+      <AdminNavigationBar />
       <Router>
-        <Route path="/" exact={true} component={LoginPage}/>
-        <Route path="/admin" component={AdminDashBoard}/>
+        <Route path="/" exact={true} component={LoginPage} />
+        <Route path="/admin" component={AdminDashBoard} />
+        <Route path="/student" component={Studentdashboard} />
       </Router>
     </div>
   );
--- src/containers/Admin/Modals/AssignClassModal/AssignClassModal.js
+++ src/containers/Admin/Modals/AssignClassModal/AssignClassModal.js
@@ -1,206 +1,219 @@
-import React, { Component } from 'react';
-import { IoIosCloseCircleOutline ,IoMdArrowDropdown, IoMdArrowDropup } from 'react-icons/io';
-import modalStyles from '../../../../assets/css/Admin/Modals/AssignClassModal/AssignClassModal.css';
+import React, { Component } from "react";
+import {
+  IoIosCloseCircleOutline,
+  IoMdArrowDropdown,
+  IoMdArrowDropup
+} from "react-icons/io";
+import modalStyles from "../../../../assets/css/Admin/Modals/AssignClassModal/AssignClassModal.css";
+
+import Select from "react-select";
+
+const optionsTeachernamelist = [
+  { value: "devindi", label: "Devindi" },
+  { value: "hiruni", label: "hiruni" },
+  { value: "kalani", label: "kalani" }
+];
+
+const optionsstsudents = [
+  { value: "devindi", label: "Devindi" },
+  { value: "hiruni", label: "hiruni" },
+  { value: "kalani", label: "kalani" },
+  { value: "lassna sanduni", label: "lassna sanduni" },
+  { value: "manorika", label: "manorika" },
+  { value: "maheshika", label: "maheshika" },
+  { value: "elanaaz", label: "elanaaz" },
+  { value: "jamila", label: "jamila" },
+  { value: "dua lipa", label: "dua lipa" }
+];
+
+const customStyles1 = {
+  option: (provided, state) => ({
+    ...provided,
+    borderBottom: "1px dotted pink",
+    color: state.isSelected ? "red" : "blue",
+    padding: 20
+  }),
+  control: () => ({
+    // none of react-select's styles are passed to <Control />
+    width: 200
+  }),
+  singleValue: (provided, state) => {
+    const opacity = state.isDisabled ? 0.5 : 1;
+    const transition = "opacity 300ms";
+
+    return { ...provided, opacity, transition };
+  }
+};
+
+const customStyles = {
+  control: () => ({
+    width: 200
+  })
+};
 
 class AssignClassModal extends Component {
-    state = {
-        isOpenDropDown:false,
-        selectedId: "ID",
-        subjectID: "subjectID",
-        isOpenDropDownSubjectID: false,
-        isOpenSelectNumber: false,
-        selectedNumber: "Number",
-        isOpenDropDownClass: false,
-        selectedClass: "Class"
+  state = {
+    isOpenDropDown: false,
+    selectedId: "ID",
+    subjectID: "subjectID",
+    isOpenDropDownSubjectID: false,
+    isOpenSelectNumber: false,
+    selectedNumber: "Number",
+    isOpenDropDownClass: false,
+    selectedClass: "Class",
+    calss: "",
+    year: "",
+    classid: ""
+  };
 
-    }
+  handleOpenDropDown = () => {
+    this.setState({ isOpenDropDown: !this.state.isOpenDropDown });
+  };
 
-    handleOpenDropDown = () => {
-        this.setState({isOpenDropDown: !this.state.isOpenDropDown});
-    }
+  handleSelectedId = event => {
+    this.setState({
+      selectedId: event.currentTarget.id,
+      isOpenDropDown: false
+    });
+  };
 
-    handleSelectedId = (event) => {
-        this.setState({
-            selectedId: event.currentTarget.id,
-            isOpenDropDown: false
-        });
-    }
+  handleOpenDropDownSubjectId = () => {
+    this.setState({
+      isOpenDropDownSubjectID: !this.state.isOpenDropDownSubjectID
+    });
+  };
 
-    handleOpenDropDownSubjectId = () => {
-        this.setState({isOpenDropDownSubjectID: !this.state.isOpenDropDownSubjectID});
-    }
+  handleSubjectId = event => {
+    this.setState({
+      subjectID: event.currentTarget.id,
+      isOpenDropDownSubjectID: false
+    });
+  };
 
-    handleSubjectId = (event) => {
-        this.setState({
-            subjectID: event.currentTarget.id,
-            isOpenDropDownSubjectID: false
-        });
-    }
+  handleOpenDropDownSelectNumber = () => {
+    this.setState({ isOpenSelectNumber: !this.state.isOpenSelectNumber });
+  };
 
-    handleOpenDropDownSelectNumber = () => {
-        this.setState({isOpenSelectNumber: !this.state.isOpenSelectNumber });
-    }
+  handleSelectNumber = event => {
+    this.setState({
+      selectedNumber: event.currentTarget.id,
+      isOpenSelectNumber: false
+    });
+  };
 
-    handleSelectNumber = (event) => {
-        this.setState({
-            selectedNumber: event.currentTarget.id,
-            isOpenSelectNumber: false
-        });
-    }
+  handleOpenDropDownClass = () => {
+    this.setState({ isOpenDropDownClass: !this.state.isOpenDropDownClass });
+  };
 
-    handleOpenDropDownClass = () => {
-        this.setState({ isOpenDropDownClass: !this.state.isOpenDropDownClass});
-    }
+  handleClass = event => {
+    this.setState({
+      selectedClass: event.currentTarget.id,
+      isOpenDropDownClass: false
+    });
+  };
 
-    handleClass = (event) => {
-        this.setState({
-            selectedClass: event.currentTarget.id,
-            isOpenDropDownClass: false
-        });
-    }
-
-    render() {
-        return(
-            <div className={modalStyles.modal}>
-                <div className={modalStyles.modal_container}>
-                <div className={modalStyles.form_container}>
-                    <h2>Assign Class For Student</h2>
-                    <div className={modalStyles.input_container}>
-                        <span>Class/Id/Year:</span>
-                        <button className={modalStyles.dropDown_button} onClick={this.handleOpenDropDown}>
-                            <span>{this.state.selectedId}</span>
-                            <div>{this.state.isOpenDropDown ? <IoMdArrowDropup size="1.5em"/> :<IoMdArrowDropdown size="1.5em"/>}</div>
-                        </button>
-                        <div className={this.state.isOpenDropDown ? modalStyles.dropDown_container : modalStyles.dropDown_container_hide}>
-                            <button
-                                id="12A2019"
-                                onClick={this.handleSelectedId}
-                            >12A2019
-                            </button>
-                            <button
-                                id="12B2019"
-                                onClick={this.handleSelectedId}
-                            >12B2019
-                            </button>
-                            <button
-                                id="12C2019"
-                                onClick={this.handleSelectedId}
-                            >
-                            12C2019
-                            </button>
-                        </div>
-                    </div>
-                    <div className={modalStyles.input_container}>
-                        <span>SubjectID:</span>
-                        <button className={modalStyles.dropDown_button} onClick={this.handleOpenDropDownSubjectId}>
-                            <span>{this.state.subjectID}</span>
-                            <div>{this.state.isOpenDropDownSubjectID ? <IoMdArrowDropup size="1.5em"/> :<IoMdArrowDropdown size="1.5em"/>}</div>
-                        </button>
-                        <div className={this.state.isOpenDropDownSubjectID ? modalStyles.dropDown_container : modalStyles.dropDown_container_hide}>
-                            <button
-                                id="12001"
-                                onClick={this.handleSubjectId}
-                            >12001
-                            </button>
-                            <button
-                                id="12002"
-                                onClick={this.handleSubjectId}
-                            >12002
-                            </button>
-                            <button
-                                id="12003"
-                                onClick={this.handleSubjectId}
-                            >
-                            12003
-                            </button>
-                        </div>
-                        </div>
-
-                        <div className={modalStyles.assign_container}>
-                            <span>Assign:</span>
-                            <div className={modalStyles.radio_container}>
-                                <input type="radio" />
-                                <span>Student</span>
-                            </div>
-                            <div className={modalStyles.radio_container}>
-                                <input type="radio" />
-                                <span>Teacher</span>
-                            </div>
-                        </div>
-
-                        <div className={modalStyles.regiNum_container}>
-                            <h3>Select Registration Numbers</h3>
-                            <div className={modalStyles.regiNum_selection_container}>
-                                <input type="radio" />
-                                <span>Assign by selecting:</span>
-                                <button className={modalStyles.dropDown_button} onClick={this.handleOpenDropDownSelectNumber}>
-                                    <span>{this.state.selectedNumber}</span>
-                                    <div>{this.state.isOpenSelectNumber ? <IoMdArrowDropup size="1.5em"/> :<IoMdArrowDropdown size="1.5em"/>}</div>
-                                </button>
-                                <div className={this.state.isOpenSelectNumber ? modalStyles.dropDown_container : modalStyles.dropDown_container_hide}>
-                                    <button
-                                        id="12345"
-                                        onClick={this.handleSelectNumber}
-                                    >12345
-                                    </button>
-                                    <button
-                                        id="24561"
-                                        onClick={this.handleSelectNumber}
-                                    >24561
-                                    </button>
-                                    <button
-                                        id="24531"
-                                        onClick={this.handleSelectNumber}
-                                    >
-                                    24531
-                                    </button>
-                                </div>
-                            </div>
-                            <div className={modalStyles.regiNum_selection_container}>
-                                <input type="radio" />
-                                <span>Assign from previous classes:</span>
-                                <button className={modalStyles.dropDown_button} onClick={this.handleOpenDropDownClass}>
-                                    <span>{this.state.selectedClass}</span>
-                                    <div>{this.state.isOpenDropDownClass ? <IoMdArrowDropup size="1.5em"/> :<IoMdArrowDropdown size="1.5em"/>}</div>
-                                </button>
-                                <div className={this.state.isOpenDropDownClass ? modalStyles.dropDown_container : modalStyles.dropDown_container_hide}>
-                                    <button
-                                        id="10A2018"
-                                        onClick={this.handleClass}
-                                    >10A2018
-                                    </button>
-                                    <button
-                                        id="10B2018"
-                                        onClick={this.handleClass}
-                                    >10B2018
-                                    </button>
-                                    <button
-                                        id="10C2018"
-                                        onClick={this.handleClass}
-                                    >
-                                    10C2018
-                                    </button>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={modalStyles.button_container}>
-                            <button>Add</button>
-                            <button>Reset</button>
-                        </div>
-                   
-                </div>
-                <button 
-                    className={modalStyles.modal_closeButton}
-                    onClick={this.props.closeModal}
-                >
-                    <IoIosCloseCircleOutline size="2em" color="black"/>
-                </button>
-                </div>
+  render() {
+    return (
+      <div className={modalStyles.modal}>
+        <div className={modalStyles.modal_container}>
+          <div className={modalStyles.form_container}>
+            <h2>Assign Class For Student</h2>
+            <div className={modalStyles.input_container}>
+              <span>class</span>
+              <select onChange={e => this.setState({ calss: e.target.value })}>
+                <option value="12" selected disabled>
+                  select class
+                </option>
+                <option value="12">12</option>
+                <option value="11">11</option>
+              </select>
             </div>
-        )
-    }
+            <div className={modalStyles.input_container}>
+              <span>Year:</span>
+
+              <select onChange={e => this.setState({ year: e.target.value })}>
+                <option value="12" selected disabled>
+                  Select year
+                </option>
+                <option value="2019">2019</option>
+                <option value="2020">2020</option>
+              </select>
+            </div>
+
+            <div className={modalStyles.input_container}>
+              <span>class id:</span>
+
+              <select
+                onChange={e => this.setState({ classid: e.target.value })}
+              >
+                <option value="12" selected disabled>
+                  Select class id
+                </option>
+                <option value="A">A</option>
+                <option value="B">B</option>
+              </select>
+            </div>
+
+            {this.state.calss && this.state.year && this.state.classid && (
+              <p>
+                class id :{" "}
+                {`${this.state.year +
+                  "/" +
+                  this.state.calss +
+                  "/" +
+                  this.state.classid}`}
+              </p>
+            )}
+
+            <div className={modalStyles.regiNum_container}>
+              <h3>Select teacher</h3>
+              <div>
+                <div className="assignteacherdiv">
+                  <Select
+                    //   defaultValue={[colourOptions[2], colourOptions[3]]}
+                    // styles={customStyles}
+
+                    style={{ width: "80000px" }}
+                    isMulti
+                    name="colors"
+                    options={optionsTeachernamelist}
+                    className="basic-multi-select"
+                    classNamePrefix="select"
+                  />
+                </div>
+              </div>
+              <div>
+                <h3>Select students</h3>
+                <div>
+                  <div className="">
+                    <Select
+                      //   defaultValue={[colourOptions[2], colourOptions[3]]}
+                      styles={customStyles}
+                      isMulti
+                      name="colors"
+                      options={optionsstsudents}
+                      className="basic-multi-select"
+                      classNamePrefix="select"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={modalStyles.button_container}>
+              <button>Add</button>
+              <button>Reset</button>
+            </div>
+          </div>
+          <button
+            className={modalStyles.modal_closeButton}
+            onClick={this.props.closeModal}
+          >
+            <IoIosCloseCircleOutline size="2em" color="black" />
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default AssignClassModal;
-
-
