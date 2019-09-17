c32028eeecef806589b8836f71de93055b471125
rizkyrajitha <[rajithagunathilake@gmail.com]>
2019-09-17 23:53:24 +0530

    router , added for user registartions

--- src/containers/Admin/AdministratorPage/AdministratorPage.js
+++ src/containers/Admin/AdministratorPage/AdministratorPage.js
@@ -1,150 +1,182 @@
-import React, { Component } from 'react';
-import { IoMdArrowDropdown, IoMdArrowDropup } from 'react-icons/io';
-import styles from '../../../assets/css/Admin/AdministratorPage/AdministratorPage.css';
-import AddClassModal from '../Modals/AddClassModal/AddClassModal';
-import AddSubjectModal from '../Modals/AddSubjectModal/AddSubjectModal';
-import AssignClassModal from '../Modals/AssignClassModal/AssignClassModal';
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
+import styles from "../../../assets/css/Admin/AdministratorPage/AdministratorPage.css";
+import AddClassModal from "../Modals/AddClassModal/AddClassModal";
+import AddSubjectModal from "../Modals/AddSubjectModal/AddSubjectModal";
+import AssignClassModal from "../Modals/AssignClassModal/AssignClassModal";
 
 class AdministratorPage extends Component {
-    state = {
-        isOpenDropDown: false,
-        selectedPerson: "Student",
-        isOpenAddNewDropDown: false,
-        selectedAddNew: "Class",
-        isOpenAddClassModal: false,
-        isOpenAddSubjectModal: false,
-        isOpenAssignClassModal: false
+  state = {
+    isOpenDropDown: false,
+    selectedPerson: "Student",
+    isOpenAddNewDropDown: false,
+    selectedAddNew: "Class",
+    isOpenAddClassModal: false,
+    isOpenAddSubjectModal: false,
+    isOpenAssignClassModal: false
+  };
+
+  handleOpenDropDown = () => {
+    this.setState({ isOpenDropDown: !this.state.isOpenDropDown });
+  };
+
+  handleSelectedPerson = event => {
+    if (event.currentTarget.id === "Student") {
+      //   this.props.history.pop();
+      this.props.history.push("/studentregis");
+    } else if (event.currentTarget.id === "Teacher") {
+      this.props.history.push("/teacherregis");
+    } else if (event.currentTarget.id === "Admin") {
+      this.props.history.push("/adminregis");
     }
 
-    handleOpenDropDown = () => {
-        this.setState({ isOpenDropDown: !this.state.isOpenDropDown});
-    }
+    this.setState({
+      selectedPerson: event.currentTarget.id,
+      isOpenDropDown: false
+    });
+  };
 
-    handleSelectedPerson = (event) => {
+  handleOpenAddNewDropDown = () => {
+    this.setState({ isOpenAddNewDropDown: !this.state.isOpenAddNewDropDown });
+  };
+
+  handleSelectedAddNew = event => {
+    this.setState({
+      selectedAddNew: event.currentTarget.id,
+      isOpenAddNewDropDown: false
+    });
+  };
+
+  handleOpenAddNewModal = () => {
+    switch (this.state.selectedAddNew) {
+      case "Class":
         this.setState({
-            selectedPerson: event.currentTarget.id,
-            isOpenDropDown: false
+          isOpenAddClassModal: true,
+          isOpenAddSubjectModal: false
         });
-    }
-
-    handleOpenAddNewDropDown = () => {
-        this.setState({ isOpenAddNewDropDown: !this.state.isOpenAddNewDropDown});
-    }
-
-    handleSelectedAddNew = (event) => {
+        break;
+      case "Subject":
         this.setState({
-            selectedAddNew: event.currentTarget.id,
-            isOpenAddNewDropDown: false
+          isOpenAddClassModal: false,
+          isOpenAddSubjectModal: true
         });
+        break;
+      default:
+        break;
     }
+  };
 
-    handleOpenAddNewModal = () => {
-        switch(this.state.selectedAddNew) {
-            case "Class":
-                this.setState({ 
-                    isOpenAddClassModal: true,
-                    isOpenAddSubjectModal: false
-                });
-                break;
-            case "Subject":
-                this.setState({ 
-                    isOpenAddClassModal: false,
-                    isOpenAddSubjectModal: true
-                });
-                break;
-            default :
-                break;
-        }
-    }
+  handleOpenAssignClassModal = () => {
+    this.setState({ isOpenAssignClassModal: true });
+  };
 
-    handleOpenAssignClassModal = () => {
-        this.setState({ isOpenAssignClassModal: true });
-    }
+  handleCloseAssignClassModal = () => {
+    this.setState({ isOpenAssignClassModal: false });
+  };
 
-    handleCloseAssignClassModal = () => {
-        this.setState({ isOpenAssignClassModal: false });
-    }
+  handleCloseAddClassModal = () => {
+    this.setState({ isOpenAddClassModal: false });
+  };
 
-    handleCloseAddClassModal = () => {
-        this.setState({ isOpenAddClassModal: false });
-    }
+  handleCloseAddSubjectModal = () => {
+    this.setState({ isOpenAddSubjectModal: false });
+  };
 
-    handleCloseAddSubjectModal = () => {
-        this.setState({ isOpenAddSubjectModal: false });
-    }
-
-    render() {
-        return (
-            <div className={styles.container} >
-                <div className={styles.admin_controller_card}>
-                    <button
-                        className={styles.admin_controller_card_button}
-                    >
-                    Registration
-                    </button>
-                    <button className={styles.dropDown_button} onClick={this.handleOpenDropDown}>
-                        <span>{this.state.selectedPerson}</span>
-                        <div>{this.state.isOpenDropDown ? <IoMdArrowDropup size="1.5em"/> :<IoMdArrowDropdown size="1.5em"/>}</div>
-                    </button>
-                    <div className={this.state.isOpenDropDown ? styles.dropDown_container : styles.dropDown_container_hide}>
-                        <button
-                            id="Student"
-                            onClick={this.handleSelectedPerson}
-                        >Student
-                        </button>
-                        <button
-                            id="Teacher"
-                            onClick={this.handleSelectedPerson}
-                        >Teacher
-                        </button>
-                        <button
-                            id="Admin"
-                            onClick={this.handleSelectedPerson}
-                        >
-                        Admin
-                        </button>
-                    </div>
-                   
-                </div>
-                <div className={styles.admin_controller_card}>
-                    <button
-                        className={styles.admin_controller_card_button}
-                        id={this.state.selectedAddNew}
-                        onClick={this.handleOpenAddNewModal}
-                    >
-                    Add New
-                    </button>
-                    <button className={styles.dropDown_button} onClick={this.handleOpenAddNewDropDown}>
-                        <span>{this.state.selectedAddNew}</span>
-                        <div>{this.state.isOpenAddNewDropDown ? <IoMdArrowDropup size="1.5em"/> :<IoMdArrowDropdown size="1.5em"/>}</div>
-                    </button>
-                    <div className={this.state.isOpenAddNewDropDown ? styles.dropDown_container : styles.dropDown_container_hide}>
-                    <button
-                        id="Class"
-                        onClick={this.handleSelectedAddNew}
-                    >Class
-                    </button>
-                    <button
-                        id="Subject"
-                        onClick={this.handleSelectedAddNew}
-                    >Subject
-                    </button>
-                </div>
-                </div>
-                <div className={styles.admin_controller_card}>
-                    <button
-                        className={styles.admin_controller_card_button}
-                        onClick={this.handleOpenAssignClassModal}
-                    >
-                    Assign
-                    </button>
-                </div>
-                {this.state.isOpenAddClassModal && <AddClassModal closeModal={this.handleCloseAddClassModal}/>}
-                {this.state.isOpenAddSubjectModal && <AddSubjectModal closeModal={this.handleCloseAddSubjectModal}/>}
-                {this.state.isOpenAssignClassModal && <AssignClassModal closeModal={this.handleCloseAssignClassModal}/>}
+  render() {
+    return (
+      <div className={styles.container}>
+        <div className={styles.admin_controller_card}>
+          <button className={styles.admin_controller_card_button}>
+            Registration
+          </button>
+          <button
+            className={styles.dropDown_button}
+            onClick={this.handleOpenDropDown}
+          >
+            <span>{this.state.selectedPerson}</span>
+            <div>
+              {this.state.isOpenDropDown ? (
+                <IoMdArrowDropup size="1.5em" />
+              ) : (
+                <IoMdArrowDropdown size="1.5em" />
+              )}
             </div>
-        )
-    }
+          </button>
+          <div
+            className={
+              this.state.isOpenDropDown
+                ? styles.dropDown_container
+                : styles.dropDown_container_hide
+            }
+          >
+            <button id="Student" onClick={this.handleSelectedPerson}>
+              Student
+            </button>
+            <button id="Teacher" onClick={this.handleSelectedPerson}>
+              Teacher
+            </button>
+            <button id="Admin" onClick={this.handleSelectedPerson}>
+              Admin
+            </button>
+          </div>
+        </div>
+        <div className={styles.admin_controller_card}>
+          <button
+            className={styles.admin_controller_card_button}
+            id={this.state.selectedAddNew}
+            onClick={this.handleOpenAddNewModal}
+          >
+            Add New
+          </button>
+          <button
+            className={styles.dropDown_button}
+            onClick={this.handleOpenAddNewDropDown}
+          >
+            <span>{this.state.selectedAddNew}</span>
+            <div>
+              {this.state.isOpenAddNewDropDown ? (
+                <IoMdArrowDropup size="1.5em" />
+              ) : (
+                <IoMdArrowDropdown size="1.5em" />
+              )}
+            </div>
+          </button>
+          <div
+            className={
+              this.state.isOpenAddNewDropDown
+                ? styles.dropDown_container
+                : styles.dropDown_container_hide
+            }
+          >
+            <button id="Class" onClick={this.handleSelectedAddNew}>
+              Class
+            </button>
+            <button id="Subject" onClick={this.handleSelectedAddNew}>
+              Subject
+            </button>
+          </div>
+        </div>
+        <div className={styles.admin_controller_card}>
+          <button
+            className={styles.admin_controller_card_button}
+            onClick={this.handleOpenAssignClassModal}
+          >
+            Assign
+          </button>
+        </div>
+        {this.state.isOpenAddClassModal && (
+          <AddClassModal closeModal={this.handleCloseAddClassModal} />
+        )}
+        {this.state.isOpenAddSubjectModal && (
+          <AddSubjectModal closeModal={this.handleCloseAddSubjectModal} />
+        )}
+        {this.state.isOpenAssignClassModal && (
+          <AssignClassModal closeModal={this.handleCloseAssignClassModal} />
+        )}
+      </div>
+    );
+  }
 }
 
-export default AdministratorPage;
+export default withRouter(AdministratorPage);
